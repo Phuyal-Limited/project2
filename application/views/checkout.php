1d49f00ba5a3e11bfea5a06a58b33075307d986d
bibekkc <dothemuzic@gmail.com>
2013-11-30 20:44:53 +0545

    some changes in checkout.php

--- application/views/checkout.php
+++ application/views/checkout.php
@@ -11,7 +11,74 @@
 						</div>
 						<div class="clear"></div>
 						<div class="col-md-12">
-							<div class="cus-details"> <!-- starts:cus details -->
+							<div class="checkout-room-details">
+								<div class="hotel-name">
+									<h1>Hotel Bidur Subedi</h1>
+									lakeside, Pokhara
+								</div>
+								<div class='clear'></div>
+
+								<div class="rounded table-wrap">
+									<div class="row"> <!-- starts:room-head -->
+										<div class="col-md-6 col-xs-4">
+											<div class="room-display-head">
+												Room Srandard
+											</div>
+										</div>
+										<div class="col-md-2 col-xs-2">
+											<div class="room-display-head">
+												No.Of Rooms
+											</div>
+										</div>
+										<div class="col-md-2 col-xs-3">
+											<div class="room-display-head">
+												Rate
+											</div>
+										</div>
+										<div class="col-md-2 col-xs-3">
+											<div class="room-display-head">
+												Total
+											</div>
+										</div>
+									</div> <!-- starts:room-head -->
+
+									<div class="row room-table-content">  <!-- starts:room-row -->
+										<div class="col-md-12">
+											<div class="room-row-wrap">
+												<div class="col-md-6 col-xs-4">
+													<div class="room-type">
+														Delux Room
+													</div>
+												</div>
+												<div class="col-md-2 col-xs-2">
+													<p>3</p>
+												</div>
+												<div class="col-md-2 col-xs-3">
+													NRs. 121.23
+												</div>
+												<div class="col-md-2 col-xs-3">
+													NRs. 1212.23
+												</div>
+											</div>
+										</div>
+										<div class="clear-big"></div>
+										<div class="col-md-12">
+											<div class="booking-totals">
+												<p>No. Of Days: <span>5</span> </p>
+												<p>Grand Total: <span>NRs 1212.32</span> </p>
+												<div class="booking-fee">
+													<p>Booking Deposite (20% of Grand Total): <span>NRs 121.23</span></p>
+												</div>
+											</div>
+										</div>
+									</div> <!-- starts:room-row -->
+								</div>
+							</div>
+							<div class="clear-big"></div>
+						</div>
+
+						<div class="col-md-12">
+							<div class="cus-details rounded"> <!-- starts:cus details -->
 								<div class="colored-head">
 									<h1>Your Details</h1>
 								</div>
@@ -49,38 +116,9 @@
 										</div>
 									</div>
 									<div class="clear"></div> <!-- clears some field below form-row -->
-								</div>
-							</div> <!-- ends:cus details -->
-							<div class="clear-big"></div>
-							<div class="cus-details"> <!-- starts:payment details -->
-								<div class="colored-head">
-									<h1>Payment Details</h1>
-								</div>
-								<div class='detail-content'>
-									<div class="clear"></div>
-									<div class="row"> <!-- starts:form-row -->
-										<div class="col-md-3 col-sm-4 col-xs-4">
-											<label>Name:</label>
-										</div>
-										<div class="col-md-6 col-sm-6 col-xs-8">
-											<input class='form-control' type="text" placeholder="Name Here">
-										</div>
-									</div> <!-- starts:form-row -->
-									<div class="clear"></div> <!-- clears some field below form-row -->
-
 									<div class="row">
 										<div class="col-md-3 col-sm-4 col-xs-4">
-											<label>Address:</label>
-										</div>
-										<div class="col-md-6 col-sm-6 col-xs-8">
-											<input class='form-control' type="text" placeholder="Address Here">
-										</div>
-									</div>
-									<div class="clear"></div> <!-- clears some field below form-row -->
-
-									<div class="row">
-										<div class="col-md-3 col-sm-4 col-xs-4">
-											<label>Expiry Date:</label>
+											<label>DOB:</label>
 										</div>
 										<div class="col-md-3 col-sm-3 col-xs-4">
 											<select class='form-control'>
@@ -108,8 +146,10 @@
 											<input class="btn btn-detault inn-button" type="submit" value="Confirm Payment">
 										</div>
 									</div>
+									<div class="clear"></div> <!-- clears some field below form-row -->
 								</div>
-							</div> <!-- ends:payment details -->
+							</div> <!-- ends:cus details -->
+							<div class="clear-big"></div>
 
 						</div>
 					</div>
