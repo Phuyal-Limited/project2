fa00bfbd8f80e022270becb530c74c346746ef01
bibekkc <dothemuzic@gmail.com>
2013-12-01 14:03:29 +0545

    Added thank you and error page!!

---
+++ application/views/error.php
@@ -0,0 +1,24 @@
+<div class="clear"></div>
+<div class="content"> <!-- starts:content -->
+	<div class="container"> <!-- starts:container -->
+		<div class="col-md-12">
+			<div class="alert alert-warning">
+				<strong>Error in payment!!</strong> lorem Ipsum dolor sit amet!! lorem Ipsum dolor sit amet!!
+			</div>
+
+			<div class="error-desc">
+				<div class="error-desc-head">
+					<h1>Error Details</h1>
+				</div>
+
+				<div class="error-desc-content">
+					<p> lorem Ipsum dolor sit amet!! lorem Ipsum dolor sit amet!! lorem Ipsum dolor sit amet!!
+					 lorem Ipsum dolor sit amet!! lorem Ipsum dolor sit amet!!
+					 lorem Ipsum dolor sit amet!! lorem Ipsum dolor sit amet!! lorem Ipsum dolor sit amet!! </p>
+				</div>
+				<div class="clear"></div>
+				<button class="btn btn-default inn-button">Back</button>
+			</div>
+		</div>
+	</div> <!-- starts:container -->
+</div> <!-- starts:content -->
---
+++ application/views/thank_you.php
@@ -0,0 +1,17 @@
+<div class="clear"></div>
+<div class="content"> <!-- starts:content -->
+	<div class="container"> <!-- starts:container -->
+		<div class="col-md-12">
+			<div class="alert alert-success">
+				<strong>Congratulation! Your payment was sent.</strong> You have sucessfully paid for this transaction.
+			</div>
+
+			<div class="thank-you-content">
+	            <p>Order details have been sent to <span>you@gmail.com</span>. Yourconfirmation code is <span>34343-343-3430</span>.</p>
+	            <p>Total transaction:<span>2342</span> </p>
+	            <p>lorem Ipsum:<span>asdf</span> </p>
+	            <p>lorem Ipsum:<span>asdf</span> </p>
+	         </div>
+		</div>
+	</div> <!-- starts:container -->
+</div> <!-- starts:content -->
