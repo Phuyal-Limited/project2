1191ed651d67e82be7890ebce6ea760df39d9aac
bibekkc <dothemuzic@gmail.com>
2013-12-07 21:30:07 +0545

    added a about page

--- application/views/about.php
+++ application/views/about.php
@@ -3,7 +3,7 @@
 	<div class="container"  <!-- starts:container -->
 		<div class="row">
 			<div class="col-md-12 col-sm-12 col-xs-12">
-				<div class="result-content"> <!-- starts:About content -->
+				<div class="about-content"> <!-- starts:About content -->
 					<ol class="breadcrumb"> <!-- starts:breadcrumbs -->
 					  <li><a href="<?php echo base_url(); ?>home">Home</a></li>
 					  <li><a href="#">Library</a></li>
@@ -12,11 +12,54 @@
 
 					<div class="row">
 						<div class="col-md-12">
+							<div class="about-head">
+								<h1>About Nepal Inn</h1>
+							</div>
+							<div class="clear"></div>
+							<div class="about-details">
+							<div class="row">
+								<div class="col-md-12">
+									<p>
+								    	Gone are the days when you had to toggle 20 browser windows to work out which hotel is cheapest when you are planning to travel. Nepal Inn scans the web for low-cost hotel, comparing the majority of the hotels based on Pokhara. This site will allows users to compare hotel prices in just a few clicks from many booking sites for over 100* hotels throughout Pokhara city. This comparison site has been formed to save the money and time of travellers. You can get information’s about the hotel and find the right place on Nepal Inn very quick and easy. So, it is considered as Nepal’s first hotel comparison site.
+								    </p>
+								</div>
+							</div>
+								<div class="row">
+									<div class="col-md-4">
+										<div class="para-title">You can find cheap hotels</div>
+										<p>
+											With Nepal Inn you can easily find your ideal hotel at the best rate. Simply enter where you want to go and your desired travel dates, and let our search engine compare accommodation prices for you. To refine your search results, simply filter by price, distance (e.g. from the bus station), user reviews, facilities and more. From budget hostels to luxury suites, Nepal Inn makes it easy to book online. You can search from a large variety of rooms and locations across Pokhara.
+										</p>
+									</div>
+									<div class="col-md-4">
+										<div class="para-title">You can Compare the prices among various hotels.</div>
+										<p>
+											Nepal Inn is a hotel price comparison site with an extensive hotel search within Pokhara. The prices shown come from numerous individual hotel booking sites. This means that while users decide on Nepal Inn which hotel best suits their needs, the booking process itself are completed with no hassle.  
+										</p>
+									</div>
+								</div>
+
+								<div class="row">
+									<div class="col-md-4">
+										<div class="para-title">Hotel reviews helps to find your ideal hotel</div>
+										<p>
+											Hotel reviews by user through their real time experience and more than thousands photos allow you to find out more about where you're travelling. To get an extended overview of a hotel property, Nepal inn shows the average rating and extensive reviews to our users.  Nepal Inn makes it easy for you to find information about the hotel meeting your expectations.
+										</p>
+									</div>
+									<div class="col-md-4">
+										<div class="para-title">Redeem with vouchers codes</div>
+										<p>
+											You can redeem the voucher codes to pay for the hotel rooms and services. In Nepal Inn, the vouchers are distributed through different channels. This is to reward the consumers having voucher codes.
+										</p>
+									</div>
+								</div>
+								
+							</div>
 							
 						</div>
 					</div>
 					
-				</div> <!-- ends:FAQ content -->
+				</div> <!-- ends:about content -->
 			</div>
 		</div>
 	</div> <!-- ends:container -->
